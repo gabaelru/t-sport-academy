8acf39b18b5ff584645ec9b3d925a45bf407192a
Ruben <ruben.barril@gmail.com>
2026-01-23 22:13:51 +0100

    enlaces inscripciones vitoria

--- src/components/sections/sedesVitoria.tsx
+++ src/components/sections/sedesVitoria.tsx
@@ -12,12 +12,12 @@ const sedesData = [
   {
     id: 'sede-olabide',
     mapsLink: 'https://maps.app.goo.gl/vLgNvTqJ2Zz5X4hQ6',
-    registrationLink: 'https://app.360player.com/registration/toquerosport/7c2015f2-1939-4b35-840a-370c035fdbd7'
+    registrationLink: 'https://app.360player.com/registration/toquerosport/1f596cd0-988d-4964-abbc-e510b3772345'
   },
   {
     id: 'sede-estadio',
     mapsLink: 'https://maps.app.goo.gl/2F8sN7RjKkY6f1t8A',
-    registrationLink: 'https://app.360player.com/registration/toquerosport/fdc87ad4-f6b4-4a1b-9fd5-da0edab0ec55'
+    registrationLink: 'https://app.360player.com/registration/toquerosport/8948160f-56dc-4d36-ba6a-acb4bdd59918'
   },
 ];
 
